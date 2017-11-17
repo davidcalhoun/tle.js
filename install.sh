eff3f7784fb62d5f3e24e407ac05de71843497b7
David Calhoun <david.b.calhoun@gmail.com>
2017-11-16 19:25:06 -0800

    remove old install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-mkdir dist || true
-
-alias babel="npm-exec babel-cli"
-
-$(npm bin)/babel tle.js --out-file dist/tle.js
-$(npm bin)/babel tle-utils.js --out-file dist/tle-utils.js
-$(npm bin)/uglifyjs dist/tle.js dist/tle-utils.js -ecma=5 -o dist/tle-min.js
