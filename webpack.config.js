3ccc60210dd8fa02c0601cabbd1ac897396a7868
David Calhoun <david.b.calhoun@gmail.com>
2017-11-20 22:04:23 -0800

    bundle dist, add webpack, tweak babel stuff

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'tle.js',
+    library: 'tle',
+    libraryTarget: 'umd',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env'
+            ],
+            plugins: [
+              require('babel-plugin-add-module-exports'),
+              require('babel-plugin-transform-es2015-modules-umd')
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    // Set a prod flag.  May use this in the future to handle errors.
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+
+    // Minifies JavaScript.
+    new webpack.optimize.UglifyJsPlugin({
+      mangle: true,
+      sourceMap: true,
+      compress: {
+        warnings: true
+      },
+      output: {
+        comments: false
+      },
+      beautify: false,
+      preserveComments: false
+    })
+  ]
+};
